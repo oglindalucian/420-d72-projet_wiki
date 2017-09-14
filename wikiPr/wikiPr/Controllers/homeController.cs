850f4806ce7260cb8bdd8683980a17b43e4e1b78
lucian o <oglindalucian@yahoo.com>
2017-09-14 11:43:41 -0400

    version14sept1

---
+++ wikiPr/wikiPr/Controllers/homeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using wikiPr.Models;
+
+namespace wikiPr.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        
+        public ActionResult Index() {
+            ViewBag.lesArticles = Article.lesArticles();
+            return View(Article.lesArticles());
+        }
+
+        public ActionResult afficher(string titre) {
+            Article a = Article.Find(titre);
+            ViewBag.letitre = titre;
+            ViewBag.lesArticles = Article.lesArticles();
+            return View(a);
+        }
+
+        public ActionResult ajouter(string titre) {
+            ViewBag.lesArticles = Article.lesArticles();
+            if (titre == null) { return View(new Article()); }
+            else return View(new Article(titre));
+
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ajouter(Article a) {
+            Article.Add(a);         
+            Article.Update(a);
+        return RedirectToAction("afficher", new { Titre = a.Titre });
+            }
+        
+
+        public ActionResult modifier(string titre) {
+            ViewBag.titre = titre;
+            ViewBag.lesArticles = Article.lesArticles();
+            return View(Article.Find(titre));
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult modifier(Article a) {
+            Article.Update(a);
+            return RedirectToAction("afficher", new { Titre = a.Titre });
+        }
+        
+        public ActionResult supprimer(string titre) {
+            ViewBag.lesArticles = Article.lesArticles();
+            Article a = Article.Find(titre);
+            return View(a);
+        }
+
+        [HttpPost]
+        public ActionResult supprimer(Article a) {
+            Article.Delete(a);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult apropos() {
+            return View();
+        }
+
+        [ChildActionOnly()]
+        public ActionResult ViewUp() {
+            return PartialView();
+        }
+
+        [ChildActionOnly()]
+        public ActionResult ViewListe() {
+            ViewBag.lesArticles = Article.lesArticles();
+            return PartialView(ViewBag.lesArticles);
+        }
+
+        //[ChildActionOnly()]
+        //public ActionResult Appercu(Article a) {
+        //    // ViewBag.lesArticles = Article.lesArticles();
+        //    ViewBag.Contenu = a.Contenu; //??????
+        //    return PartialView(ViewBag.Contenu);
+        //}
+
+        //public ActionResult Apercu(string s) {
+        //    Apercu ap = new Models.Apercu(s);
+        //    ViewBag.Contenu = ap.Content; //??????
+        //    return View(new Apercu(s));
+        //}
+
+        //public JsonResult verifierTitre(string Titre) {
+        //    var validateName = Article.FirstOrDefault
+        //                        (x => x.Titre == Titre);
+        //    if (validateName != null) {
+        //        return Json(false, JsonRequestBehavior.AllowGet);
+        //    }
+        //    else {
+        //        return Json(true, JsonRequestBehavior.AllowGet);
+        //    }
+        //}
+
+    }
+}
+ 
+ 
---
+++ wikiPr/wikiPr/Models/Article.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wikiPr.Models {
+    public class Article {
+       // [Uniqueness("Titre")]
+        //   [Unique(ErrorMessage = "This already exist !!")]
+     //   [Remote("verifierTitre", "home", ErrorMessage = "Titre Already Exist.")]
+        [Required(ErrorMessage = "Le titre est requis ")]
+        public string Titre { get; set; }
+
+        public string Contenu { get; set; }
+
+        public DateTime DateModification { get; set; }
+
+        public int Revision { get; set; }
+
+        public int IdContributeur { get; set; }
+
+        public Article() {}
+
+        public Article(string titre) {
+            this.Titre = titre;
+        }
+
+        public static List<Article> lesArticles() {
+            string chConnexion = ConfigurationManager.ConnectionStrings["WikiCon"].ConnectionString;
+            SqlConnection connexion = new SqlConnection(chConnexion);
+            string requete = "dbo.GetTitresArticles";
+            SqlCommand commande = new SqlCommand(requete, connexion);
+            commande.CommandType = System.Data.CommandType.StoredProcedure;
+            List<Article> maListe = new List<Article>();
+            try {
+                connexion.Open();
+                SqlDataReader dr = commande.ExecuteReader();
+                while (dr.Read()) {
+                    Article a = new Article {
+                        Titre = (string)dr["Titre"],
+                        Contenu = (string)dr["Contenu"],
+                        DateModification = (DateTime)dr["DateModification"],
+                        Revision = (int)dr["Revision"],
+                        IdContributeur = (int)dr["IdContributeur"]
+                    };
+                    maListe.Add(a);
+                }
+
+                dr.Close();
+                return maListe;
+            }
+            catch (Exception e) {
+                string Message = e.Message;
+            }
+            finally {
+                connexion.Close();
+            }
+            return null;
+        }
+        
+
+        public static void Add(Article a) {
+            string chConnexion = ConfigurationManager.ConnectionStrings["WikiCon"].ConnectionString;
+            SqlConnection connexion = new SqlConnection(chConnexion);
+            string requete = "INSERT INTO Article (Titre, Contenu, DateModification, Revision, IdContributeur) VALUES ('"
+            + a.Titre + "','" + a.Contenu + "','" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "'," + a.Revision + "," + 1 + ")";
+            SqlCommand commande = new SqlCommand(requete, connexion);
+            commande.CommandType = System.Data.CommandType.Text;
+            try {
+                connexion.Open();
+                commande.ExecuteNonQuery();
+                connexion.Close();
+            }
+            catch { }
+        }
+
+        public static Article Find (string titre) {
+            string chConnexion = ConfigurationManager.ConnectionStrings["WikiCon"].ConnectionString;
+            SqlConnection connexion = new SqlConnection(chConnexion);
+            string requete = "SELECT * FROM Article WHERE Titre = '" + titre + "'";
+            SqlCommand commande = new SqlCommand(requete, connexion);
+            commande.CommandType = System.Data.CommandType.Text;
+            try {
+                connexion.Open();
+                SqlDataReader dr = commande.ExecuteReader();
+                dr.Read();
+                Article a = new Article {
+                    Titre = (string)dr["Titre"],
+                    Contenu = (string)dr["Contenu"],
+                    DateModification = (DateTime)dr["DateModification"],
+                    Revision = (int)dr["Revision"],
+                    IdContributeur = (int)dr["IdContributeur"]                    
+                };                
+                return a;
+            }
+            catch (Exception e) {
+                string Message = e.Message;
+            }
+            finally {
+                connexion.Close();
+            }
+            return null;
+        }        
+
+        public static void Update(Article a) {
+            string chConnexion = ConfigurationManager.ConnectionStrings["WikiCon"].ConnectionString;
+            SqlConnection connexion = new SqlConnection(chConnexion);
+            string requete = "UPDATE Article SET Titre = '" + a.Titre +
+                "', DateModification = '" + DateTime.Now + "', Contenu = '" + a.Contenu
+                + "' WHERE Titre = '" + a.Titre + "'";
+              
+            SqlCommand commande = new SqlCommand(requete, connexion);
+            commande.CommandType = System.Data.CommandType.Text;
+            try {
+                connexion.Open();
+                commande.ExecuteNonQuery();
+                connexion.Close();
+            }
+            catch { }
+        }
+
+        public static void Delete(Article a) {
+            string chConnexion = ConfigurationManager.ConnectionStrings["WikiCon"].ConnectionString;
+            SqlConnection connexion = new SqlConnection(chConnexion);
+            string requete = "DELETE FROM Article WHERE Titre = '" + a.Titre + "'";
+            SqlCommand commande = new SqlCommand(requete, connexion);
+            commande.CommandType = System.Data.CommandType.Text;
+            try {
+                connexion.Open();
+                commande.ExecuteNonQuery();
+                connexion.Close();
+            }
+            catch { }
+        }
+
+        //public class UniquenessAttribute : ValidationAttribute {
+        //    string attri { get; set; }
+        //    public UniquenessAttribute(string attri) {
+        //        this.attri = attri;
+        //    }
+
+        //    protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+        //        var property = validationContext.ObjectType.GetProperty(attri);
+        //        if (property == null) {
+        //            return new ValidationResult(
+        //                string.Format("Unknown property: {0}", attri)
+        //            );
+        //        }
+
+        //        if (attri == "Titre" && Find(value.ToString()) != null) {
+        //            return new ValidationResult(validationContext.DisplayName + " existe deja!");
+        //        }
+
+        //        return null;
+        //    }
+        //}
+    }
+}
