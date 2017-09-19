4b8bdc436e8984e9836ad5830fb1413a5f9aa06e
lucian o <oglindalucian@yahoo.com>
2017-09-18 20:58:57 -0400

    18-2

--- wikiPr/wikiPr/Models/Utilisateur.cs
+++ wikiPr/wikiPr/Models/Utilisateur.cs
@@ -20,42 +20,49 @@ namespace wikiPr.Models {
         enum Langues : byte { fr, en, es };
         
         [Key]
-        [Required]
+       // [Required]
         public int Id { get; set; }
 
        // [Display(Name = "Prenom", ResourceType = typeof(ResourceView))]
-        [Required(ErrorMessage = "Indiquez votre prenom")]
+        [Display(Name = "Prenom")]
+        [Required]
+        //[Required(ErrorMessage = "Indiquez votre prenom")]
         public string Prenom { get; set; }
 
-       // [Display(Name = "Nom de famille", ResourceType = typeof(ResourceView))]
-        [Required(ErrorMessage = "Indiquez votre nom de famille")]
+        // [Display(Name = "Nom de famille", ResourceType = typeof(ResourceView))]
+        [Display(Name = "Nom de famille")]
+        // [Required(ErrorMessage = "Indiquez votre nom de famille")]
+        [Required]
         public string NomFamille { get; set; }
 
         // [Uniqueness("Courriel")]
-     //   [Display(Name = "Adresse courriel", ResourceType = typeof(ResourceView))]
-        [Required(ErrorMessage = "Indiquez votre courriel")]
+        //   [Display(Name = "Adresse courriel", ResourceType = typeof(ResourceView))]
+        [Display(Name = "Adresse courriel")]   
+        //[Required(ErrorMessage = "Indiquez votre courriel")]
         [DataType(DataType.EmailAddress)]
+        [Required]
         public string Courriel { get; set; }
 
-        [Required(ErrorMessage = "Indiquez le mot de passe"), StringLength(50, MinimumLength=6)]
+        [Required]
+        // [Required(ErrorMessage = "Indiquez le mot de passe"), StringLength(50, MinimumLength=6)]
         [DataType(DataType.Password)]
-     //   [Display(Name = "Mot de passse", ResourceType = typeof(ResourceView))]
+        //   [Display(Name = "Mot de passse", ResourceType = typeof(ResourceView))]
+        [Display(Name = "Mot de passse")]
         public string MDP { get; set; }
 
-        //[Required(ErrorMessage = "Confirmez le mot de passe!"), StringLength(50, MinimumLength = 6)]
-        //[DataType(DataType.Password)]
-        //[Display(Name = "Confirmez le mot de passse:")]
-        //[Compare("MDP", ErrorMessage = "Le mot de passe et la confirmation ne correspondent pas.")]
-        //public string Confirmation { get; set; }
+        [Required(ErrorMessage = "Confirmez le mot de passe!"), StringLength(50, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmez le mot de passse:")]
+        [Compare("MDP", ErrorMessage = "Le mot de passe et la confirmation ne correspondent pas.")]
+        public string Confirmation { get; set; }
 
-        [Required(ErrorMessage = "Choisissez la langue")]
-       // [Display(Name = "Langue", ResourceType = typeof(ResourceView))]
+        [Required]
+        // [Required(ErrorMessage = "Choisissez la langue")]
+        // [Display(Name = "Langue", ResourceType = typeof(ResourceView))]
+        [Display(Name = "Langue")]
         public string Langue { get; set; }
 
-        //14. Pour faciliter la conversion entre un UtilisateurInscription et un Utilisateur, 
-        //ajoutez un constructeur 
-        //dans la classe Utilisateur qui prend en argument le modèle de présentation.
-
+       
         public Utilisateur() { }
 
         public Utilisateur(UtilisateurInscription ui) {
